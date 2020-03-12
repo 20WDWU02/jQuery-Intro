b593cae7249a699f979f991d1e4cb9d1c2e930b9
RichardHpa <richard.m.hpa@gmail.com>
2020-03-13 09:31:54 +1300

    added a small bit of jquery

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// alert('javascript has loaded');
+
+// $
+// jQuery
+
+// $(selector).effect()
+
+
+
+$('#blueBox').hide();
+
+$('#greenBox').click(function(){
+    $('#blueBox').show();
+})
+
+$('#redBox').click(function(){
+    $('#blueBox').hide();
+})
