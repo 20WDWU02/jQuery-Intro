38b0d874be0f0891ee70b7a7b9afacc5511d806d
RichardHpa <richard.m.hpa@gmail.com>
2020-03-16 11:53:31 +1300

    added jquery demos

--- js/script.js
+++ js/script.js
@@ -4,15 +4,37 @@
 // jQuery
 
 // $(selector).effect()
-
+            // .action()
+            // .event()
 
 
 $('#blueBox').hide();
 
 $('#greenBox').click(function(){
     $('#blueBox').show();
-})
+});
 
 $('#redBox').click(function(){
     $('#blueBox').hide();
+});
+
+
+$('#modalExample1Button').click(function(){
+    // console.log('button has been clicked');
+    $('#modalExample1').removeClass('hidden').addClass('flex');
+    // $('#modalExample1').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#modalExample1Close').click(function(){
+    // console.log('close button has been clicked');
+    $('#modalExample1').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+
+$('#navListToggleButton').click(function(){
+    // console.log('clicked');
+    // $('#navList').toggle();
+    $('#navList').slideToggle(300);
 })
